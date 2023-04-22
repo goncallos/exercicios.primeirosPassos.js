203fc8a296fe96f856670add4e098dd13d471f05
goncallos <leandrooliveiragoncallos@gmail.com>
2023-04-21 23:38:34 -0300

    ex001/1

--- ex001.js
+++ ex001.js
@@ -1,4 +1,15 @@
 function escrevaMeuNome(nome) {
     console.log('Meu nome é ' + nome);
 }
-escrevaMeuNome('leandro')
+escrevaMeuNome('leandro');
+escrevaMeuNome('murilo')
+
+function verificaridade(idade) {
+    if (idade >= 18) {
+        console.log('maior');
+    }else {
+        console.log('menor')
+    }
+}
+
+verificaridade(12)
