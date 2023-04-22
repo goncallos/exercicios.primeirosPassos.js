1e11423c1dcc3f4eff71929b11dc99c0faf7804c
goncallos <leandrooliveiragoncallos@gmail.com>
2023-04-21 23:06:41 -0300

    funcões imc

---
+++ funções.js
@@ -0,0 +1,10 @@
+function main () {
+}
+
+(function main() {
+    const peso = 75;
+    const altura = 1.75;
+
+    const imc = calcularImc(peso, altura);
+    console.log(classificarimc(imc));
+})();
